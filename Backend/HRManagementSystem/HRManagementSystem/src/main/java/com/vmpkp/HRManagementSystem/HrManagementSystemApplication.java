47a78e2f046cb5bf2b89a73f5f9cf8bb04095f2d
Vedant1112 <codegenix.solutions@gmail.com>
2024-04-13 18:10:30 +0530

    testing whether git is ignoring property file

--- Backend/HRManagementSystem/HRManagementSystem/src/main/java/com/vmpkp/HRManagementSystem/HrManagementSystemApplication.java
+++ Backend/HRManagementSystem/HRManagementSystem/src/main/java/com/vmpkp/HRManagementSystem/HrManagementSystemApplication.java
@@ -2,11 +2,18 @@ package com.vmpkp.HRManagementSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-public class HrManagementSystemApplication {
-
+public class HrManagementSystemApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(HrManagementSystemApplication.class);
+	}
 	public static void main(String[] args) {
+
 		SpringApplication.run(HrManagementSystemApplication.class, args);
 	}
 
